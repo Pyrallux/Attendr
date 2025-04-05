284b1104f5e5e3bf0aea4eb8910bbbacc523f0e3
alvinmoy <alvinsanmoy@gmail.com>
2025-04-05 13:48:31 -0500

    p

--- Frontend/app/profileStyles.ts
+++ Frontend/app/profileStyles.ts
@@ -7,7 +7,7 @@ export const profileStyles = StyleSheet.create({
     backgroundColor: "#4b4b4b",
   },
   content: {
-    paddingTop: 200,
+    paddingTop: 240,
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
@@ -16,6 +16,8 @@ export const profileStyles = StyleSheet.create({
   bg: {
     flex: 1,
     backgroundColor: "#292929",
+    height: "100%",
+    width: "100%",
   },
   nameText: {
     color: "white",
@@ -26,5 +28,27 @@ export const profileStyles = StyleSheet.create({
     color: "gray",
     fontSize: 16,
     fontFamily: "Jersey10",
+  },
+  view: {
+    paddingTop: 100,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
+  box: {
+    height: 128,
+    flexDirection: "column",
+    width: 375,
+    gap: 25,
+    backgroundColor: "#4b4b4b",
+    justifyContent: "center",
+    alignItems: "center",
+    display: "flex",
+    borderRadius: 44,
+  },
+  flex: {
+    flex: 1,
+    display: "flex",
   },
 });

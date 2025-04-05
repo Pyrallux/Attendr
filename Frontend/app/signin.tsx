433cafe79cfecc205ad281ea0b653bba397566c3
Pyrallux <nnnevwa630@gmail.com>
2025-04-05 10:29:52 -0500

    Change bottom bar

--- Frontend/app/signin.tsx
+++ Frontend/app/signin.tsx
@@ -11,31 +11,32 @@ export default function SignUp() {
   };
   return (
     <>
-      <View style={signupStyles.bg}>
-        <View style={signupStyles.logo}>
-          <Text style={signupStyles.logoText}>Attendr</Text>
-        </View>
-        <View style={signupStyles.content}>
-          <View style={signupStyles.box}>
-            <Text style={signupStyles.header}>Sign In</Text>
-
-            <TextInput
-              style={signupStyles.border}
-              maxLength={20}
-              placeholder="Username"
-              onChangeText={(text) => setUserInput(text)}
-            ></TextInput>
-
-            <TextInput
-              style={signupStyles.border}
-              maxLength={20}
-              placeholder="Password"
-              onChangeText={(text) => setUserInput(text)}
-            ></TextInput>
-            <Button onPress={handleSubmit} title="submit"></Button>
-          </View>
-        </View>
-      </View>
+      <TextInput
+        maxLength={20}
+        placeholder="First Name"
+        onChangeText={(text) => setUserInput(text)}
+      ></TextInput>
+      <TextInput
+        maxLength={20}
+        placeholder="Last Name"
+        onChangeText={(text) => setUserInput(text)}
+      ></TextInput>
+      <TextInput
+        maxLength={20}
+        placeholder="Username"
+        onChangeText={(text) => setUserInput(text)}
+      ></TextInput>
+      <TextInput
+        maxLength={20}
+        placeholder="Email"
+        onChangeText={(text) => setUserInput(text)}
+      ></TextInput>
+      <TextInput
+        maxLength={20}
+        placeholder="Password"
+        onChangeText={(text) => setUserInput(text)}
+      ></TextInput>
+      <Button onPress={handleSubmit} title="Sign In"></Button>
     </>
   );
 }

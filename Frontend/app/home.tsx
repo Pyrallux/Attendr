dc333f072263f4aa27ca46009bf5c6d5791e15bf
VincentVord <vvor3@ku.edu>
2025-04-05 14:32:41 -0500

    Get location on homepage

--- Frontend/app/home.tsx
+++ Frontend/app/home.tsx
@@ -1,8 +1,11 @@
 import BottomBar from "@/components/BottomBar/BottomBar";
 import { homeStyles } from "./homeStyles";
-import { Text, View } from "react-native";
-
+import { Text, View, StyleSheet} from "react-native";
 import { useFonts } from "expo-font";
+import * as Location from 'expo-location';
+import { useEffect} from "react";
+import React from 'react'
+import MapView from 'react-native-maps'
 
 export default function Home() {
   const [fontsLoaded] = useFonts({
@@ -12,10 +15,41 @@ export default function Home() {
   if (!fontsLoaded) {
     return undefined;
   }
+
+  useEffect(() => {
+    getUserLocation(); //call the location function
+  }, []);
+
+  const getUserLocation = async () => {
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      alert('Permission denied');
+      return;
+    }
+  
+    const location = await Location.getCurrentPositionAsync({});
+    console.log('User location:', location.coords);
+    return location.coords; // contains latitude and longitude
+  };
+
+  // const GoogleMap = () => {
+  //   return (
+  //     <MapView
+  //       style={styles.mapView}/>
+  //   );
+  // }
+
+  // const styles = StyleSheet.create({
+  //   mapView: {
+  //     flex: 1
+  //   }
+  // })
+
   return (
     <>
       <View style={homeStyles.bg}>
         <View style={homeStyles.bg}>
+
           <View style={homeStyles.view}>
             <View style={homeStyles.box}>
               <Text style={[homeStyles.timeText, { fontFamily: "Jersey10" }]}>

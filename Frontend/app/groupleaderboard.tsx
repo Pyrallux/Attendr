691e7646a3f6d7c455834b3635ff21a94d0e7f8a
Pyrallux <nnnevwa630@gmail.com>
2025-04-06 06:26:35 -0500

    start leaderboard file

--- Frontend/app/groupleaderboard.tsx
+++ Frontend/app/groupleaderboard.tsx
@@ -2,44 +2,40 @@ import BottomBar from "@/components/BottomBar/BottomBar";
 import { groupsStyles } from "../styles/groupsStyles";
 import { View, Button } from "react-native";
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { getGroups, getUserDetail } from "@/api/api";
+import { getGroupDetail, getGroups, getUserDetail } from "@/api/api";
 import { useContext, useEffect, useState } from "react";
 import { AppContext } from "./_layout";
 import { Redirect, useRouter } from "expo-router";
 
-interface Group {
-  id: number;
-  name: string;
-  members: number[];
-  admin: string;
+interface Member {
+  user_id: number;
+  username: string;
+  score: number
 }
 
-export default function Groups() {
-  const [groupList, setGroupList] = useState<Group[]>([]);
-  const { userId, setActiveGroupId } = useContext(AppContext);
+export default function GroupLeaderboard() {
+  const [memberList, setMemberList] = useState<Member[]>([]);
+  const { userId, activeGroupId } = useContext(AppContext);
   const router = useRouter();
 
   const { data: groupData } = useQuery({
     queryKey: ["groupList"],
-    queryFn: () => getGroups(),
+    queryFn: () => getGroupDetail(activeGroupId),
   });
+  const { mutateAsync: getUserDetailMutation } = useMutation({
+    mutationFn: (id: number) => getUserDetail(id),
+  })
 
   useEffect(() => {
     if (groupData) {
-      setGroupList([
-        ...groupData.filter((g: Group) => g.members.includes(userId)),
-      ]);
+      let member_list: Member[] = [];
+      for (let i=0; i < groupData.members.length; i++) {
+        let userData = await getUserDetailMutation(groupData[i])
+        member_list.push({user_id: userData.user_id, username: userData.username, score: userData.score});
+      }
+      setMemberList([...member_list])
     }
   }, [groupData]);
-
-  const handleGroupClick = (id: number) => {
-    setActiveGroupId(id);
-    router.navigate("/groupleaderboard");
-  };
-  const handleEditGroupClick = (id: number) => {
-    setActiveGroupId(id);
-    router.navigate("/editgroup");
-  };
 
   if (userId === -1) {
     return <Redirect href="/signin"></Redirect>;
@@ -47,34 +43,14 @@ export default function Groups() {
   return (
     <>
       <View style={groupsStyles.bg}>
-        <View style={groupsStyles.bg}>
-          <View style={groupsStyles.container}>
-            <View style={groupsStyles.content}>
-              {groupList.map((g: Group, id) => {
-                return (
-                  <View key={id}>
-                    <Button
-                      title={g.name}
-                      onPress={() => handleGroupClick(g.id)}
-                    />
-                    <Button
-                      title="Edit"
-                      onPress={() => handleEditGroupClick(g.id)}
-                    />
-                  </View>
-                );
-              })}
-              <View>
-                <Button
-                  title="Add New Group +"
-                  onPress={() => router.navigate("/addgroup")}
-                ></Button>
-              </View>
-            </View>
-          </View>
-        </View>
-      </View>
+        {memberList.map((m: member, i) =>
+        <View key={i}>
+          <Text>#{i}</Text>
+       <Text>{m.username}</Text>
+       <Text>{m.score}</Text>
+       </View>
       <BottomBar></BottomBar>
+      </View>
     </>
   );
 }

ea2f52d2c650edfa25dbade6f65243bcc48a5a9c
Pyrallux <nnnevwa630@gmail.com>
2025-04-06 04:16:34 -0500

    Add Course Page

--- Frontend/app/addcourse.tsx
+++ Frontend/app/addcourse.tsx
@@ -16,7 +16,7 @@ import {
   useMutation,
   useQueryClient,
 } from "@tanstack/react-query";
-import { addGroup, getUsers } from "@/api/api";
+import { addCourse, addGroup, getUsers } from "@/api/api";
 import { Controller, useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -24,59 +24,103 @@ import { useRouter } from "expo-router";
 import { useFonts } from "expo-font";
 import BottomBar from "@/components/BottomBar/BottomBar";
 import CheckBox from "expo-checkbox";
-import DateTimePicker from "@react-native-community/datetimepicker";
+import DateTimePicker, {
+  DateTimePickerEvent,
+} from "@react-native-community/datetimepicker";
+import SelectionMap from "../components/SelectionMap";
+import { format } from "date-fns";
 
-interface Group {
+interface Course {
   id?: number;
   name: string;
-  members: number[];
-  admin: string;
-}
-
-interface FormData {
-  name: string;
+  time: string;
+  start_date: string;
+  end_date: string;
+  user_id: number;
+  days: number[];
+  days_attended: number;
+  days_missed: number;
+  latitude: number;
+  longitude: number;
 }
 
 export default function AddCourse() {
+  const [courseName, setCourseName] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [time, setTime] = useState(new Date());
+
   const [dayList, setDayList] = useState<number[]>([]);
+  const [showLocationPicker, setShowLocationPicker] = useState<boolean>(false);
+  const [courseLat, setCourseLat] = useState<number>(0);
+  const [courseLong, setCourseLong] = useState<number>(0);
   const router = useRouter();
   const { user, userId } = useContext(AppContext);
   const queryClient = useQueryClient();
 
-  const { mutateAsync: addGroupMutation } = useMutation({
+  const { mutateAsync: addCourseMutation } = useMutation({
     mutationKey: ["SignInGetUsers"],
-    mutationFn: (group: Group) => addGroup(group),
+    mutationFn: (course: Course) => addCourse(course),
   });
 
-  const schema = yup.object().shape({
-    name: yup.string().max(20).required("*Name is Required"),
-    time: yup.string().required("*Time input is required"),
-    days: yup.array().of(yup.number().max(7)).min(1),
-    start_date: yup.date().min(new Date()).required("*Start date is required"),
-    end_date: yup.date().required("*End date is required"),
-  });
+  // const schema = yup.object().shape({
+  //   name: yup.string().max(20).required("*Name is Required"),
+  //   time: yup.string().required("*Time input is required"),
+  //   days: yup.array().of(yup.number().max(7)).min(1).required(),
+  //   start_date: yup.date().min(new Date()).required("*Start date is required"),
+  //   end_date: yup.date().required("*End date is required"),
+  // });
   const {
     control,
     handleSubmit,
     formState: { errors },
+    setValue,
   } = useForm({
     mode: "onSubmit",
-    resolver: yupResolver(schema),
+    // resolver: yupResolver(schema),
   });
 
-  const onSubmit = async (formData: FormData) => {
-    let newGroup: Group = {
-      name: formData.name,
-      members: [userId],
-      admin: user,
+  const onSubmit = async (formData: any) => {
+    let newCourse: Course = {
+      name: courseName,
+      time: format(time, "HH:mm:ss"),
+      start_date: format(startDate, "yyyy-MM-dd"),
+      end_date: format(endDate, "yyyy-MM-dd"),
+      user_id: userId,
+      days: dayList,
+      days_attended: 0,
+      days_missed: 0,
+      latitude: courseLat,
+      longitude: courseLong,
     };
-    await addGroupMutation(newGroup);
-    queryClient.invalidateQueries(["groupList"]);
-    router.navigate("/groups");
+    await addCourseMutation(newCourse);
+    queryClient.invalidateQueries(["getCourseList"]);
+    router.navigate("/schedule");
   };
 
-  const handleEditTime = (time: string) => {
-    // do something
+  const handleName = (text: string) => {
+    if (text) {
+      setCourseName(text);
+    }
+  };
+
+  const handleStartDate = (event: DateTimePickerEvent, selectedDate?: Date) => {
+    if (selectedDate) {
+      setStartDate(selectedDate);
+    }
+  };
+
+  const handleEndDate = (event: DateTimePickerEvent, selectedDate?: Date) => {
+    if (selectedDate) {
+      setEndDate(selectedDate);
+    }
+  };
+
+  const handleEditTime = (event: DateTimePickerEvent, selectedTime?: Date) => {
+    console.log(selectedTime);
+    if (selectedTime) {
+      setTime(selectedTime);
+    }
   };
 
   const handleClickCheck = (id: number) => {
@@ -113,123 +157,55 @@ export default function AddCourse() {
                 Enter a New Course
               </Text>
 
-            {errors.time && (
-              <Text style={[signinStyles.text, { color: "red" }]}>
-                {errors.time.message}
-              </Text>
-            )}
+              <Text>Name</Text>
+              <Controller
+                control={control}
+                name="start_date"
+                render={({ field: { onChange, value } }) => (
+                  <TextInput value={courseName} onChangeText={handleName} />
+                )}
+              />
 
-            <Controller
-              control={control}
-              name="start_date"
-              render={({ field: { onChange, value } }) => (
-                <DateTimePicker
-                  value={new Date()}
-                  mode="date"
-                  onChange={() => handleEditTime}
-                />
-              )}
-            />
-            {errors.start_date && (
-              <Text style={[signinStyles.text, { color: "red" }]}>
-                {errors.start_date.message}
-              </Text>
-            )}
+              <Text>Start Date</Text>
+              <Controller
+                control={control}
+                name="start_date"
+                render={({ field: { onChange, value } }) => (
+                  <DateTimePicker
+                    value={startDate}
+                    mode="date"
+                    onChange={handleStartDate}
+                  />
+                )}
+              />
 
-            <Controller
-              control={control}
-              name="end_date"
-              render={({ field: { onChange, value } }) => (
-                <DateTimePicker
-                  value={new Date()}
-                  mode="date"
-                  onChange={() => handleEditTime}
-                />
-              )}
-            />
-            {errors.end_date && (
-              <Text style={[signinStyles.text, { color: "red" }]}>
-                {errors.end_date.message}
-              </Text>
-            )}
+              <Text>End Date</Text>
+              <Controller
+                control={control}
+                name="end_date"
+                render={({ field: { onChange, value } }) => (
+                  <DateTimePicker
+                    value={endDate}
+                    mode="date"
+                    onChange={handleEndDate}
+                  />
+                )}
+              />
 
-            <Controller
-              control={control}
-              name="start_date"
-              render={({ field: { onChange, value } }) => (
-                <DateTimePicker
-                  value={new Date()}
-                  mode="date"
-                  onChange={() => handleEditTime}
-                />
-              )}
-            />
-            {errors.start_date && (
-              <Text style={[signinStyles.text, { color: "red" }]}>
-                {errors.start_date.message}
-              </Text>
-            )}
-
-            <Controller
-              control={control}
-              name="end_date"
-              render={({ field: { onChange, value } }) => (
-                <DateTimePicker
-                  value={new Date()}
-                  mode="date"
-                  onChange={() => handleEditTime}
-                />
-              )}
-            />
-            {errors.end_date && (
-              <Text style={[signinStyles.text, { color: "red" }]}>
-                {errors.end_date.message}
-              </Text>
-            )}
-
-            <TouchableOpacity
-              onPress={() => {
-                handleSubmit(onSubmit)();
-              }}
-              style={{ marginTop: 20 }}
-            >
-              <ImageBackground
-                source={require("../assets/images/submitButton.png")} // replace with your actual path
-                style={{
-                  width: 200,
-                  height: 60,
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-                imageStyle={{ borderRadius: 10 }} // optional rounded corners
-              >
-                <Text
-                  style={{
-                    color: "white",
-                    fontSize: 20,
-                    fontFamily: "Jersey10",
-                    paddingBottom: 10,
-                  }}
-                >
-                  Submit
-                </Text>
-              )}
+              <Text>Time</Text>
               <Controller
                 control={control}
                 name="time"
                 render={({ field: { onChange, value } }) => (
                   <DateTimePicker
-                    value={new Date()}
+                    value={time}
                     mode="time"
-                    onChange={() => handleEditTime}
+                    onChange={handleEditTime}
                   />
                 )}
               />
-              {errors.time && (
-                <Text style={[signinStyles.text, { color: "red" }]}>
-                  {errors.time.message}
-                </Text>
-              )}
+
+              <Text>Recurrence Schedule:</Text>
               <Controller
                 control={control}
                 name="days"
@@ -274,10 +250,15 @@ export default function AddCourse() {
                 )}
               />
 
-              {errors.time && (
-                <Text style={[signinStyles.text, { color: "red" }]}>
-                  {errors.time.message}
-                </Text>
+              <Button
+                title="Location Picker"
+                onPress={() => setShowLocationPicker(!showLocationPicker)}
+              ></Button>
+              {showLocationPicker && (
+                <SelectionMap
+                  onFindLat={setCourseLat}
+                  onFindLong={setCourseLong}
+                ></SelectionMap>
               )}
 
               <TouchableOpacity

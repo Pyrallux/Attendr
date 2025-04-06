4b647150b66509beb28e15f1ef85da27a5c538ab
Pyrallux <nnnevwa630@gmail.com>
2025-04-06 07:49:37 -0500

    Update Homepage

--- Frontend/app/index.tsx
+++ Frontend/app/index.tsx
@@ -8,13 +8,57 @@ import { AppContext } from "./_layout";
 import { Redirect } from "expo-router";
 import { LinearGradient } from "expo-linear-gradient";
 import DistanceMap from "@/components/DistanceMap";
+import { getCourses, getEvents } from "@/api/api";
+import { useMutation } from "@tanstack/react-query";
+
+interface Event {
+  id?: number;
+  name: string;
+  datetime: string;
+  user_id: number;
+}
 
 export default function Home() {
   const { userId, isAtEvent } = useContext(AppContext);
+  const [latitude, setLatitude] = useState(-1);
+  const [longitude, setLongitude] = useState(-1);
+  const [sortedEvents, setSortedEvents] = useState<Event[]>([]);
 
   const [fontsLoaded] = useFonts({
     Jersey10: require("../assets/fonts/Jersey10-Regular.ttf"),
   });
+
+  const { mutateAsync: getCoursesMutation } = useMutation({
+    mutationKey: ["getCourseList"],
+    mutationFn: () => getCourses(),
+    onSuccess: (data) => {
+      let course = data.filter((c: any) => sortedEvents[0].name === c.name);
+      setLatitude(course[0].latitude);
+      setLongitude(course[0].longitude);
+    },
+  });
+
+  const { mutateAsync: getEventsMutation } = useMutation({
+    mutationKey: ["getEventList"],
+    mutationFn: () => getEvents(),
+    onSuccess: (data) => {
+      let user_events: any = data.filter((e: any) => e.user_id === userId);
+      let sorted_events: any = user_events.sort((a: any, b: any) => {
+        if (new Date(a.datetime) < new Date(b.datetime)) {
+          return -1;
+        }
+        if (new Date(a.datetime) > new Date(b.datetime)) {
+          return 1;
+        }
+        setSortedEvents([...sorted_events]);
+        getCoursesMutation();
+      });
+    },
+  });
+
+  useEffect(() => {
+    getEventsMutation();
+  }, []);
 
   // TODO Get the top class from the stack
   const handleCheckIn = () => {
@@ -27,9 +71,10 @@ export default function Home() {
   if (userId === -1) {
     return <Redirect href="/signin"></Redirect>;
   }
-  // TODO remove set lat and long values
+
   return (
     <>
+      <Button title="Check In"></Button>
       <View style={homeStyles.bg}>
         <LinearGradient
           colors={["#0f9679", "#292929"]}
@@ -60,7 +105,7 @@ export default function Home() {
               <Button title="Check In" onPress={handleCheckIn}></Button>
             )}
           </View>
-          <DistanceMap eventLat={10} eventLong={20}></DistanceMap>
+          <DistanceMap eventLat={latitude} eventLong={longitude}></DistanceMap>
           <BottomBar></BottomBar>
         </LinearGradient>
       </View>
